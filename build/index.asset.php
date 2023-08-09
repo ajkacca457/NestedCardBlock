0d0f0cf6ccf3f7b9b994d187bfe702df3b2b5ede
Avijit karmaker <ajkacca@gmail.com>
2023-08-09 14:01:36 +0300

    add styling to appender button

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '48931b2070531e96ec13');
+<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '195e3348ac0f5ce447ba');
