c4ee2a2ff0adf875c44c2ebf1a63ab6ae1c1ac79
Avijit karmaker <ajkacca@gmail.com>
2023-08-09 12:35:37 +0300

    update text style

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '60ef6675aa09df809520');
+<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'db90ac4703b39ebbcfdd');
